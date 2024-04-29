1db63589d0969ae6595f98d1d03e364e842b3f66
Igor Eduardo <igorercl@gmail.com>
2024-04-29 00:52:52 -0300

    atualizando

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -10,25 +10,28 @@ public class FilmeController : ControllerBase
     private static List<Filmes> filmes = new List<Filmes>();
     private static int Id = 1;
 
+// FUNÇAO CRIADA PARA ADICIONAR FILME COM METODO POST, A PARTIR DO FROMBODY E DA LISTA PRIVADA DE FILMES E DO ID CRIADO
     [HttpPost]
-    public void adicionarfilme([FromBody] Filmes filme)
+    public IActionResult Adicionarfilme([FromBody] Filmes filme)
     {
         filme.Id = Id++;
         filmes.Add(filme);
-        Console.WriteLine(filme.Titulo);
-        Console.WriteLine(filme.Genero);
+          return CreatedAtAction(nameof(BuscarFilme), new {id = filme.Id },filme);
     }
-
+//FUNÇAO CRIADA PARA RETONAR O PARAMETRO DE EXIBIÇAO DE FILMES SE TIVER 100 ELE TEM COMO PADRAO MOSTRAR O FILME 0 ATE 0 10 OU
+// O QUE O USUARIO QUISER A PARTIR DO FROMQUERY ?12&&
     [HttpGet]
     public IEnumerable<Filmes> ListarFilmesAdicionados([FromQuery]int skip= 0,[FromQuery] int take = 10)
     {
         return filmes.Skip(skip).Take(take);
     }
-
+//FUNÇAO IACTION PARA RETORNAR O STATUS CODE COM BASE NA BUSCA DO ID DO FILME QUE FOI ADIOCIONADO NA LISTA DE FILMES
     [HttpGet("{id}")]
-    public Filmes? BuscarFilme(int id)
+    public IActionResult BuscarFilme(int id)
     {
-        return filmes.FirstOrDefault(filme => filme.Id == id);
+        var filme = filmes.FirstOrDefault(filme => filme.Id == id);
+        if (filme == null) return NotFound();
+        return Ok(filme);
     }
 }
 
