bf741d6649848dfe22269a0e282e3c6f349d853e
Igor Eduardo <igorercl@gmail.com>
2024-04-29 00:15:31 -0300

    Adicionar arquivos de projeto.

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FilmesAPI.Models;
+
+namespace FilmesAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase
+{
+    private static List<Filmes> filmes = new List<Filmes>();
+    private static int Id = 1;
+
+    [HttpPost]
+    public void adicionarfilme([FromBody] Filmes filme)
+    {
+        filme.Id = Id++;
+        filmes.Add(filme);
+        Console.WriteLine(filme.Titulo);
+        Console.WriteLine(filme.Genero);
+    }
+
+    [HttpGet]
+    public IEnumerable<Filmes> ListarFilmesAdicionados([FromQuery]int skip= 0,[FromQuery] int take = 10)
+    {
+        return filmes.Skip(skip).Take(take);
+    }
+
+    [HttpGet("{id}")]
+    public Filmes? BuscarFilme(int id)
+    {
+        return filmes.FirstOrDefault(filme => filme.Id == id);
+    }
+}
+
+
